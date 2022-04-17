a80b714b6e3ff8c398d84c323e6ac78ed149274a
ibroman <ibroman79@gmail.com>
2022-04-17 22:52:42 +0300

    Ajoutez des fichiers projet.

---
+++ Abstraite_Interface/Program.cs
@@ -0,0 +1,31 @@
+﻿using Abstraite_Interface.modele;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraite_Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r1 = new Rectangle(7, 5); Rectangle r2 = new Rectangle(17, 12); Rectangle r3 = new Rectangle(13, 9);
+            Cercle c1 = new Cercle(5.6F); Cercle c2 = new Cercle(15F); Cercle c3 = new Cercle(35F);
+            List<Figure> figures = new List<Figure>();
+            figures.Add(r1); figures.Add(r2); figures.Add(r3); figures.Add(c1); figures.Add(c2); figures.Add(c3);
+            foreach(Figure f in figures)
+            {
+                //Console.WriteLine(f);
+                if (f is Cercle)
+                {
+                    Cercle c = (Cercle)f;
+                    Console.WriteLine(c.getRayon());
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Abstraite_Interface/modele/Cercle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraite_Interface.modele
+{
+    class Cercle : Figure
+    {
+        private float rayon;
+
+        public Cercle(float rayon)
+        {
+            this.rayon = rayon;
+        }
+        public override float Perimetre()
+        {
+            return 2 * 3.14F * this.rayon;
+        }
+
+        public override float Surface()
+        {
+            return 3.14F * this.rayon * this.rayon;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder message = new StringBuilder();
+            message.AppendFormat("Cercle\nRayon={0}\n" +
+                "Surface={1}\nPerimetre={2}\n", this.rayon,
+                 this.Surface(), this.Perimetre());
+            return message.ToString();
+        }
+
+        public float getRayon()
+        {
+            return this.rayon;
+        }
+    }
+}
